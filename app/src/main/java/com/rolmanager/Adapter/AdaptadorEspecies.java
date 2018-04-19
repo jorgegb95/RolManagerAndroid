592c700e2613ca2974894da4921559140caa6e38
jorgegb95 <jgb040195@gmail.com>
2018-04-19 17:04:23 +0200

    Control lista de especies

--- app/src/main/java/com/rolmanager/Adapter/AdaptadorEspecies.java
+++ app/src/main/java/com/rolmanager/Adapter/AdaptadorEspecies.java
@@ -1,4 +1,33 @@
 package com.rolmanager.Adapter;
 
-public class AdaptadorEspecies {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.rolmanager.R;
+import com.rolmanager.database.Especies;
+
+public class AdaptadorEspecies extends ArrayAdapter<Especies>{
+    private Especies lista[];
+
+    public AdaptadorEspecies(Activity context, Especies[] datos){
+        super(context, R.layout.activity_especies, datos);
+        lista=datos;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View item=inflater.inflate(R.layout.textolistview, null);
+
+        TextView lblNombre=(TextView) item.findViewById(R.id.LblNombre);
+        lblNombre.setText(lista[position].getNombre());
+
+        TextView lblRelacion=(TextView) item.findViewById(R.id.LblRelacion);
+        lblNombre.setText(lista[position].getRelacion());
+
+        return item;
+    }
 }
