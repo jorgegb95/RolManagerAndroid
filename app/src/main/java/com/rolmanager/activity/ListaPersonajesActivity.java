293585a3ebf8746900fbf0d59ef2a7649bcbd09e
jorgegb95 <jgb040195@gmail.com>
2018-05-16 22:37:59 +0200

    Cambio de boton para nuevo personaje

--- app/src/main/java/com/rolmanager/activity/ListaPersonajesActivity.java
+++ app/src/main/java/com/rolmanager/activity/ListaPersonajesActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
